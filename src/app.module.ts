f1ecf1e0d2b83f34a7a000b1eb90e1e013179c96
bsrisompong <bsrisompong@gmail.com>
2021-05-01 21:50:40 +0700

    database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'admin',
+      // entities: [],
+      autoLoadEntities: true,
+      // ^ don't use the same produuction environment beccause it will always migrate databases.
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
