ca3215745d4b0b1ca343a065b0b52d319d19f552
bsrisompong <bsrisompong@gmail.com>
2021-05-20 08:18:25 +0700

    create order module

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Post,
+  Query,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AuthGuard } from '../auth/auth.guard';
+import { Order } from './order.entity';
+import { OrderService } from './order.service';
 
-@Controller('order')
-export class OrderController {}
+@UseInterceptors(ClassSerializerInterceptor)
+@UseGuards(AuthGuard)
+@Controller('orders')
+export class OrderController {
+  constructor(private orderService: OrderService) {}
+
+  @Get('')
+  async all(@Query('page') page = 1, take = 10) {
+    // return this.orderService.all(['order_items']);
+    return this.orderService.paginate(page, take, ['order_items']);
+  }
+
+  // TODO : create OrderCreateDto
+  @Post()
+  async create(@Body() body): Promise<Order> {
+    return this.orderService.create(body);
+  }
+}
--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AbstractService } from 'src/common/abstract.service';
+import { PaginatedResult } from 'src/common/paginated-result.interface';
+import { Repository } from 'typeorm';
+import { Order } from './order.entity';
 
 @Injectable()
-export class OrderService {}
+export class OrderService extends AbstractService {
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+  ) {
+    super(orderRepository);
+  }
+
+  async paginate(
+    page = 1,
+    take = 15,
+    relations = [],
+  ): Promise<PaginatedResult> {
+    const { data, meta } = await super.paginate(page, take, relations);
+
+    return {
+      data: data.map((order: Order) => ({
+        id: order.id,
+        name: order.name,
+        email: order.email,
+        created_at: order.created_at,
+        total: order.total,
+        order_items: order.order_items,
+      })),
+      meta,
+    };
+  }
+}
